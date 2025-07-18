780bac4af0d7859d403a79e1c581ab32d008e3ef
Nandishwar04 <77493143+Nandishwar04@users.noreply.github.com>
2025-07-18 11:29:15 +0530

    Update applicationController.js

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -1,7 +1,14 @@
 const Application = require("../models/Application");
 
-exports.submitApplication = async (req, res) => {
-  const newApp = new Application(req.body);
-  await newApp.save();
-  res.status(201).json({ message: "Application submitted" });
+const submitApplication = async (req, res) => {
+  try {
+    const { name, email, resume, jobId } = req.body;
+    const application = new Application({ name, email, resume, jobId });
+    await application.save();
+    res.status(201).json({ message: "Application submitted" });
+  } catch (error) {
+    res.status(500).json({ message: "Submission failed", error });
+  }
 };
+
+module.exports = { submitApplication };
