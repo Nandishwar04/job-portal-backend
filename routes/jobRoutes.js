1ad3aeb0cf51824e46367efd7772f3a93be2d8b7
Nandishwar04 <77493143+Nandishwar04@users.noreply.github.com>
2025-07-18 11:29:33 +0530

    Update jobRoutes.js

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { getJobs, getJobById } = require("../controllers/jobController");
 
+// GET /api/jobs/
 router.get("/", getJobs);
+
+// GET /api/jobs/:id
 router.get("/:id", getJobById);
 
 module.exports = router;
