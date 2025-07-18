bcb2c724caf1b820b665d0739622647140c49986
Nandishwar04 <77493143+Nandishwar04@users.noreply.github.com>
2025-07-18 11:29:48 +0530

    Update applicationRoutes.js

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { submitApplication } = require("../controllers/applicationController");
 
-
+// POST /api/applications/
 router.post("/", submitApplication);
 
 module.exports = router;
